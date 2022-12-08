446e0f46a1b02a7072e3add77abf95323a151b35
daniel-727 <zaoster777@gmail.com>
2022-12-08 10:46:02 -0600

    finished multiple pointer solution for are_there_duplicates.js

--- are_there_duplicates.js
+++ are_there_duplicates.js
@@ -31,11 +31,34 @@
 
 /*-----O(n) Multiple pointers solution-----*/
 
-function are_there_duplicates(){
+function are_there_duplicates(...args){// we use the ...spread operator to create an array of the values inside the arguments object
+
+    console.log(args);
+
+    //we need to sort the args or we won't be able to implement an efficient multiple pointers solution, sort mutates the original array so if you console log before and after this args.sort() line. the array will be different.
+    args.sort();
+    
+    if (args.length < 2){ //if there is only 1 value in the array, then there are no duplicates
+        return false;
+    }
+    let start = args[0];
+    let next = args[1];
+
+
+    //since args is sorted, if there are any duplicate values, they should be right next to eachother
+    for (let i = 1; i < args.length; i++){
+        if (start === next) { //if the value at index 0 is equal to the value at index 1 then there are duplicates
+            return true;
+        }
+        start = args[i];
+        next = args[i+1];
+    }
+
+    //after the loop if true hasn't been returned yet, then there are no duplicates
+    return false;
 
 }
 
-
-let result = are_there_duplicates(1,33,4,'x','1');
+let result = are_there_duplicates('a','b','b','c');
 
 result ? console.log('true') : console.log('false');
